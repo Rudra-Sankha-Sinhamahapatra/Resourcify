82626e796df90f9995eee814a80089148733e646
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-27 22:16:42 +0530

    updating vite config for dockerize environment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Allow access from outside the container
+    port: 5173,      // Default port Vite uses
+    strictPort: true, // Prevent Vite from trying to use another port if 5173 is taken
+  },
+});
